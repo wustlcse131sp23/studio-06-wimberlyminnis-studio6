2550ba67a5908ebcf92a75066a1edcb49338dbe9
jackwimberly <jackwimberly@10.232.178.56>
2023-03-09 15:47:44 -0600

    studio 6

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -12,10 +12,12 @@ public class RecursiveMethods {
 	 *         ...)
 	 */
 	public static double geometricSum(int n) {
-		
-			// FIXME compute the geometric sum for the first n terms recursively
+		if(n==0) {
 			return 0;
-		
+		}
+		double k = 1/Math.pow(2, n) + geometricSum(n-1);
+			return k;
+			// FIXME compute the geometric sum for the first n terms recursively
 	}
 
 	/**
@@ -27,10 +29,10 @@ public class RecursiveMethods {
 	 * @return greatest common divisor of p and q
 	 */
 	public static int gcd(int p, int q) {
-		
-			// FIXME compute the gcd of p and q using recursion
-			return 0;
-		
+		if(q==0) {
+			return p;
+		}
+		return gcd(q, p%q); 
 	}
 
 	
@@ -42,12 +44,17 @@ public class RecursiveMethods {
 	 * @return an array with the same data as the input but it reverse order
 	 */
 	public static int[] toReversed(int[] array) {
-		
-			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
-		
+		int[]reversed = new int[array.length];
+		return mirrored(array, reversed, 0);
 	}
 
+	public static int[] mirrored(int[] array, int[] reversed, int index) {
+		if(index>=array.length) {
+			return reversed;
+		}
+		reversed[array.length-index-1] = array[index];
+		return mirrored(array, reversed, index +1);
+	}
 	/**
 	 * @param xCenter                       x-coordinate of the center of the circle
 	 *                                      at the current depth
@@ -59,7 +66,6 @@ public class RecursiveMethods {
 	 */
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius,
 			double radiusMinimumDrawingThreshold) {
-		
 		// FIXME
 	}
 
